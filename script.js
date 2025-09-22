1407d36b8d6ffb13d04396dc020313ba1d4e8cf3
PRIME DNH <nannuji72@gmail.com>
2025-09-22 10:35:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// Set a specific username and password
+const correctUsername = "prime";
+const correctPassword = "1234";
+
+// Track login attempts
+let attempts = 0;
+const maxAttempts = 4;
+
+const loginBtn = document.getElementById('loginBtn');
+
+loginBtn.addEventListener('click', function() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if(username === correctUsername && password === correctPassword){
+        alert("WELCOME CHHOKRE!");
+        window.location.href = "page1.html"; // Redirect to second page
+    } else {
+        attempts++;
+        alert(`Incorrect username or password! Attempt ${attempts} of ${maxAttempts}`);
+        
+        if(attempts >= maxAttempts){
+            alert("You have reached the maximum number of attempts. Login disabled.");
+            loginBtn.disabled = true;
+        }
+    }
+});
+
+
+
